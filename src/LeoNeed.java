dc95536156060ebeb8300cfb7bf0c61bd877bff6
Xtramemium <93661341+Xtramemium@users.noreply.github.com>
2024-03-11 01:20:54 +0300

    My commit

--- src/LeoNeed.java
+++ src/LeoNeed.java
@@ -1,5 +1,6 @@
 public class LeoNeed {
     public static void main(String[] args) {
-        System.out.println("Вот они изменения");
+        System.out.println("Вот они изменения"); // а тут не удалил
+        System.out.println("тут вот добавил ");
     }
 }
